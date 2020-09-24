c8594299ad6970fd3efaaf23e1436131deba0a1d
Denniz741 <70800391+Denniz741@users.noreply.github.com>
2020-09-24 21:21:14 +0600

    Update 7_ex.py

--- 7_ex.py
+++ 7_ex.py
@@ -12,23 +12,4 @@ print(get_summ('five',5))
 
 
 
-    #try:
-
-
-    #num_one = int()
-    #
-    # num_two = int()
-    #return sum(num_one, num_tow)
-
-#get_summ(1,2)
-
-
-
-
-
-#print(sum(num_one, num_tow))
-#print(c)
-
-
-    #except ValueError:
-     #   print('Значение аргумента не корректно.')
+    
